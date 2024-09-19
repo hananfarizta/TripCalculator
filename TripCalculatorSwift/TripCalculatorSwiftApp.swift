96598178a8df370e40e898787833e099e473e3e4
hananfarizta <hananfariztaa@gmail.com>
2024-09-19 15:05:39 +0700

    Initial Commit

---
+++ TripCalculatorSwift/TripCalculatorSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripCalculatorSwiftApp.swift
+//  TripCalculatorSwift
+//
+//  Created by Hanan Farizta on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TripCalculatorSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
